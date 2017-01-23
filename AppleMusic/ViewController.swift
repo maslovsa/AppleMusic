122fca7ebc89c36def87027cc6a72d35f52a3677
Maslov Sergey <maslovsa@gmail.com>
2017-01-23 10:25:31 +0400

    Initial Commit

---
+++ AppleMusic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleMusic
+//
+//  Created by Maslov Sergey on 23.01.17.
+//  Copyright © 2017 Maslov Sergey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
