d127e3b297ddb10ac5ff210c0ba55e4c096f58b8
Maslov Sergey <maslovsa@gmail.com>
2017-01-23 11:11:07 +0400

    fix cell view

--- AppleMusic/SearchItemCell.swift
+++ AppleMusic/SearchItemCell.swift
@@ -11,7 +11,7 @@ import SDWebImage
 
 class SearchItemCell: UITableViewCell {
     static let Identifier = "SearchItemCell"
-    static let CellHeight = 100
+    static let CellHeight = 60 + 8 + 8
     
     var searchItem: SearchItem!
     
@@ -19,11 +19,8 @@ class SearchItemCell: UITableViewCell {
     fileprivate var collectionName = UILabel()
     fileprivate var trackName = UILabel()
     fileprivate var artwork = UIImageView()
-    //    var artistName: String = ""
-    //    var collectionName: String = ""
-    //    var trackName: String = ""
-    //    var artworkUrl100: String = ""
     fileprivate let cornerRadius: CGFloat = 10
+    fileprivate let artistNameTextLeftOffset = 76
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
@@ -45,28 +42,50 @@ class SearchItemCell: UITableViewCell {
     }
     
     func localInit() {
-        
         artwork.contentMode = .scaleAspectFit
         artwork.layer.cornerRadius = cornerRadius
         artwork.layer.masksToBounds = true
         addSubview(artwork)
         artwork.snp.makeConstraints { (make) -> Void in
             make.top.left.equalTo(self).offset(8)
-            make.width.height.equalTo(40)
+            make.width.height.equalTo(60)
         }
         
         artistName.textAlignment = .left
         artistName.font = UIFont.systemFont(ofSize: 13, weight: UIFontWeightRegular)
         artistName.lineBreakMode = .byTruncatingTail
-        artistName.textColor = UIColor.gray
+        artistName.textColor = UIColor.black
         artistName.backgroundColor = UIColor.clear
         addSubview(artistName)
         artistName.snp.makeConstraints { (make) -> Void in
-            make.left.equalTo(self).offset(50)
+            make.left.equalTo(self).offset(artistNameTextLeftOffset)
             make.top.equalTo(self).offset(8)
             make.right.equalTo(self)
         }
         
+        collectionName.textAlignment = .left
+        collectionName.font = UIFont.systemFont(ofSize: 13, weight: UIFontWeightRegular)
+        collectionName.lineBreakMode = .byTruncatingTail
+        collectionName.textColor = UIColor.darkGray
+        collectionName.backgroundColor = UIColor.clear
+        addSubview(collectionName)
+        collectionName.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(self).offset(artistNameTextLeftOffset)
+            make.top.equalTo(self).offset(30)
+            make.right.equalTo(self)
+        }
+        
+        trackName.textAlignment = .left
+        trackName.font = UIFont.systemFont(ofSize: 13, weight: UIFontWeightRegular)
+        trackName.lineBreakMode = .byTruncatingTail
+        trackName.textColor = UIColor.gray
+        trackName.backgroundColor = UIColor.clear
+        addSubview(trackName)
+        trackName.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(self).offset(artistNameTextLeftOffset)
+            make.top.equalTo(self).offset(50)
+            make.right.equalTo(self)
+        }
     }
     
     func configure(_ searchItem: SearchItem) {
@@ -77,5 +96,7 @@ class SearchItemCell: UITableViewCell {
         }
         
         artistName.text = searchItem.artistName
+        collectionName.text = searchItem.collectionName
+        trackName.text = searchItem.trackName
     }
 }
